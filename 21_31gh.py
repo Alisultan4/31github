d8a3658e696d7a8a5552024f71aa4771bd7ae75c
Alisultan4 <codeexplorer08@gmail.com>
2023-12-26 13:58:51 +0600

    прол

--- 21_31gh.py
+++
@@ -1,6 +0,0 @@
-#I thought I was a cool programmer after I did 2 months of python bootcamp
-#but it seems like it's not true
-#im not even an entry level of python developer
-#maybe if I finished my courses then I could already had an internship
-# i cant even splve basic questions 
-# like what im gonna do 
